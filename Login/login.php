dd4d607945b3e1d084e6a60b386b3b8c210ed241
Werundy <106776945+Werundy@users.noreply.github.com>
2022-06-06 14:54:29 -0600

    Add files via upload

---
+++ Login/login.php
@@ -0,0 +1,39 @@
+<?php
+
+    include 'ConexionDB.php';
+
+    $con = new conexion();
+    $estadocon = $con->getCon();
+
+    $usuario = $_POST['txtUsuario'];
+    $pass = $_POST['txtPass'];
+
+    $query = "EXEC Acce.UDP_tblUsuarios_Validacion '$usuario','$pass'";
+    $result = sqlsrv_query($estadocon, $query);
+
+    if(sqlsrv_has_rows($result) != 1){
+        PRINT '<script>';
+        PRINT 'alert("El usuarioo contraseña son incorrectos")';
+        PRINT '</script>';
+
+        PRINT '<script>';
+        PRINT 'window.location= "pages-login.php" ';
+        PRINT '</script>';
+    }
+    else{
+        session_start();
+        $row = sqlsrv_fetch_array($result);
+        PRINT '<script>';
+        PRINT 'window.location= "../Menu/PantallasMostrar/blank.php" ';
+        PRINT '</script>';
+
+    }
+
+        PRINT '<script>';
+        PRINT 'window.location= "pages-login.php" ';
+        PRINT '</script>';
+    
+   
+
+
+?>
