5f3f1420548e6434877659b739d3420a3e714936
Werundy <106776945+Werundy@users.noreply.github.com>
2022-06-09 09:37:36 -0600

    Add files via upload

---
+++ Menu/PantallasMostrar/comprasDetallesJson.php
@@ -0,0 +1,30 @@
+<?php
+
+    include '../../assets/conexion/ConexionDB.php';
+    $con = new conexion();
+    $estadocon = $con->getCon();
+    $Orden = $_GET['NoOrden'];
+
+    $response = array();
+    $query="EXEC Inv.UDP_tblCompraDetalles_MostrarRecibo '$Orden'";
+    $result = sqlsrv_prepare($estadocon,$query);
+
+       if(sqlsrv_execute($result)){
+           $response['response'] = array();
+           while($row = sqlsrv_fetch_array($result)){
+               array_push(
+                   $response['response'],
+                   array(
+                       "compraDetalleIdentificacion"=>$row['CompDe_Id'],"compraNoOrden"=>$row['Comp_NoOrden'],"ingredienteIdentificacion"=>$row['Ingr_Id'], "ingredienteDescripcion"=>$row['Ingr_Descripcion'], "compraDetallePrecio"=>$row['CompDe_PrecioCompra'], "compraDetalleCantidad"=>$row['CompDe_Cantidad'],"compraIVA"=>$row['Comp_IVA']
+                   )
+               );
+           }
+       }
+       else{
+            $response['response']= sqlsrv_errors();
+       }
+
+    
+      echo json_encode($response);
+
+?>
